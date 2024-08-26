b527983a7ef9ea7e06fede7910929816c8ab0bb1
Mahesh-Wijerathna <maheshtharanga49@gmail.com>
2024-08-26 20:11:10 +0530

    Updated with postman Requests

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,8 @@
 const neo4j = require('neo4j-driver');
 
 const driver = neo4j.driver(
-    process.env.NEO4J_URI || 'neo4j://localhost',
-    neo4j.auth.basic(process.env.NEO4J_USER || 'neo4j', process.env.NEO4J_PASSWORD || 'password')
+    'neo4j+s://c266e885.databases.neo4j.io' || process.env.NEO4J_URI || 'neo4j://localhost',
+    neo4j.auth.basic( process.env.NEO4J_USER || 'neo4j', process.env.NEO4J_PASSWORD || 'QXOpkuM9sdtcpUyvjH1WzKXs1y7f8tQ7n9DLLgVkAS8')
 );
 
 const session = driver.session();
